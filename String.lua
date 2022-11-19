7a6bb75ea099fad2155ced25adc9832fc73d3546
AkiKonani <aki.konani@gmail.com>
2022-11-19 20:02:22 +0000

    A solution for when many lines are joined

--- String.lua
+++ String.lua
@@ -1,7 +1,17 @@
 local function startsWith(string, startingWithString)
-    return string.sub(string, 1, string.len(startingWithString)) == startingWithString
+  return string.sub(string, 1, string.len(startingWithString)) == startingWithString
+end
+
+local function join(delimiter, strings)
+  local result = strings[1] or ''
+  for index = 2, #strings do
+    local string = strings[index]
+    result = result .. delimiter .. string
+  end
+  return result
 end
 
 String = {
-    startsWith = startsWith
+  startsWith = startsWith,
+  join = join
 }
