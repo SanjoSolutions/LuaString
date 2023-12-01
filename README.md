37f38d9552a2afab14b6586723a84f71543e24b8
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-12-01 18:16:59 +0100

    Updates

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ A library for working with strings. This library can save add-on developers some
 * **String.startsWith**: a function that checks if a string starts with a string.
 * **String.join**: a function that joins multiple strings with a delimiter.
 * **String.split**: a function that splits a string by a delimiter.
+
+## How to use
+
+Please see "[How to use Library](https://github.com/SanjoSolutions/LuaLibrary#how-to-use)" for how to use this library.
--- String.lua
+++ String.lua
@@ -1,26 +1,34 @@
-local function startsWith(string, startingWithString)
-  return string.sub(string, 1, string.len(startingWithString)) == startingWithString
-end
+local addOnName = 'String'
+local version = '2.0.1'
 
-local function join(delimiter, strings)
-  local result = strings[1] or ''
-  for index = 2, #strings do
-    local string = strings[index]
-    result = result .. delimiter .. string
+if _G.Library then
+  if not Library.isRegistered(addOnName, version) then
+    --- @class String
+    local String = {}
+
+    function String.startsWith(string, startingWithString)
+      return string.sub(string, 1, string.len(startingWithString)) == startingWithString
+    end
+
+    function String.join(delimiter, strings)
+      local result = strings[1] or ''
+      for index = 2, #strings do
+        local string = strings[index]
+        result = result .. delimiter .. string
+      end
+      return result
+    end
+
+    function String.split(delimiter, string)
+      if _G.strsplittable then
+        return strsplittable(delimiter, string)
+      else
+        return { strsplit(delimiter, string) }
+      end
+    end
+
+    Library.register(addOnName, version, String)
   end
-  return result
+else
+  error(addOnName .. ' requires Library. It seems absent.')
 end
-
-local function split(delimiter, string)
-  if _G.strsplittable then
-    return strsplittable(delimiter, string)
-  else
-    return { strsplit(delimiter, string) }
-  end
-end
-
-String = {
-  startsWith = startsWith,
-  join = join,
-  split = split
-}
